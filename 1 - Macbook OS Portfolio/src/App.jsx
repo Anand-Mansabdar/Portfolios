2d19e35859f497893bb8a362c2f3df74cd27ed94
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-11-23 18:49:34 +0530

    scaffold: Implemented Skills Section and HOC

--- 1 - Macbook OS Portfolio/src/App.jsx
+++ 1 - Macbook OS Portfolio/src/App.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import Welcome from "#components/Welcome";
 import Navbar from "#components/Navbar";
 import Dock from "#components/Dock";
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+import Terminal from "#windows/Terminal";
 
-
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal />
     </main>
   );
 };
