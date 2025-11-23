2e02a8b84639199f1070ccd8e20fc3f8a5902a34
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-11-23 12:10:06 +0530

    scaffold: Implemented the Dock Section

--- 1 - Macbook OS Portfolio/src/App.jsx
+++ 1 - Macbook OS Portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Welcome from "#components/Welcome";
 import Navbar from "#components/Navbar";
+import Dock from "#components/Dock";
+
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   );
 };
