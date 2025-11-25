5c87054df0ce515eb11a84ed234b7c12f4b2b9b0
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-11-25 20:49:58 +0530

    scaffold: Implemented Gallery Section and almost finished Project

--- 1 - Macbook OS Portfolio/src/App.jsx
+++ 1 - Macbook OS Portfolio/src/App.jsx
@@ -11,6 +11,8 @@ import Finder from "#windows/Finder";
 import Text from "#windows/Text";
 import ImageContent from "#windows/ImageContent";
 import Contact from "#windows/Contact";
+import Home from "#components/Home";
+import Photos from "#windows/Photos";
 
 gsap.registerPlugin(Draggable);
 const App = () => {
@@ -27,6 +29,8 @@ const App = () => {
       <Text />
       <ImageContent />
       <Contact />
+      <Home />
+      <Photos />
     </main>
   );
 };
