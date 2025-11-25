0a4893d3c04d7af12d1c6d759764ebf54c73d0b4
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-11-25 20:10:08 +0530

    scaffold: Implemented Contact Section

--- 1 - Macbook OS Portfolio/src/App.jsx
+++ 1 - Macbook OS Portfolio/src/App.jsx
@@ -10,6 +10,7 @@ import Resume from "#windows/Resume";
 import Finder from "#windows/Finder";
 import Text from "#windows/Text";
 import ImageContent from "#windows/ImageContent";
+import Contact from "#windows/Contact";
 
 gsap.registerPlugin(Draggable);
 const App = () => {
@@ -25,6 +26,7 @@ const App = () => {
       <Finder />
       <Text />
       <ImageContent />
+      <Contact />
     </main>
   );
 };
